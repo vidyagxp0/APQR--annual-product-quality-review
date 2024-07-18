bb9bf2f0cbd6b8a2642adfe1ee4171d4e52e321c
gauravidya <gaurav.meena@vidyagxp.com>
2024-07-18 15:57:47 +0530

    created bottomheader and table on dashboard

--- frontEnd/src/Component/BottomHeader.jsx
+++ frontEnd/src/Component/BottomHeader.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export default function BottomHeader() {
+  const navigate = useNavigate();
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="flex justify-between  bg-slate-300 px-10">
+        {/* <div className="flex items-center">
+          <img src="./gxplogo.png" className="h-10 mr-2" alt="" />
+        </div> */}
+        <div className="flex gap-4 items-center justify-center bg-slate-300 py-2 text-blue-500 font-medium">
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) =>
+              `text-blue-500 ${
+                isActive
+                  ? "text-blue-600 underline text-xl underline-offset-8"
+                  : "underline-offset-4 hover:underline"
+              }`
+            }
+          >
+            Dashboard
+          </NavLink>
+          <NavLink
+            to="/analytics"
+            className={({ isActive }) =>
+              `text-blue-500 ${
+                isActive
+                  ? "text-blue-600 underline text-xl underline-offset-8"
+                  : "underline-offset-4 hover:underline"
+              }`
+            }
+          >
+            Analytics
+          </NavLink>
+          <NavLink
+            to="/notification"
+            className={({ isActive }) =>
+              `text-blue-500 ${
+                isActive
+                  ? "text-blue-600 underline  text-xl underline-offset-8"
+                  : "underline-offset-4 hover:underline"
+              }`
+            }
+          >
+            Notification
+          </NavLink>
+        </div>
+        <div className="p-4">
+          <button
+            onClick={() => navigate("/new-pqr")}
+            className="bg-green-600 text-white font-bold py-2 px-4 rounded-lg  hover:bg-green-700"
+          >
+            New APQR
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontEnd/src/pages/Dashboard.jsx
+++ frontEnd/src/pages/Dashboard.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Header from "../Component/Header";
+import BottomHeader from "../Component/BottomHeader";
 
 export default function Dashboard() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Header />
+      <BottomHeader />
+      <div className="container mt-5">
+        <table className="table-auto w-full">
+          <thead className="bg-slate-500 text-white">
+            <tr>
+              <th className="px-4 py-2 border-r-2">Column 1</th>
+              <th className="px-4 py-2 border-r-2">Column 2</th>
+              <th className="px-4 py-2 border-r-2">Column 3</th>
+              <th className="px-4 py-2 border-r-2">Column 4</th>
+              <th className="px-4 py-2 border-r-2">Column 5</th>
+              <th className="px-4 py-2 border-r-2 ">Column 6</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-slate-100">
+              <td className="px-4 py-2 border-r-2">Row 1</td>
+              <td className="px-4 py-2 border-r-2">Row 2</td>
+              <td className="px-4 py-2 border-r-2">Row 3</td>
+              <td className="px-4 py-2 border-r-2">Row 4</td>
+              <td className="px-4 py-2 border-r-2">Row 5</td>
+              <td className="px-4 py-2 border-r-2">Row 6</td>
+            </tr>
+            <tr className="bg-white">
+              <td className="px-4 py-2 border-r-2">Row 1</td>
+              <td className="px-4 py-2 border-r-2">Row 2</td>
+              <td className="px-4 py-2 border-r-2">Row 3</td>
+              <td className="px-4 py-2 border-r-2">Row 4</td>
+              <td className="px-4 py-2 border-r-2">Row 5</td>
+              <td className="px-4 py-2 border-r-2">Row 6</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
