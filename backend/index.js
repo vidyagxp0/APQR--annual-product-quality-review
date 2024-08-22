a3c6977bad48984b8459839f89dc3645505c7ac6
gaurav <gaurav.meena@vidyagxp.com>
2024-08-22 10:59:32 +0530

    proper backend setup and database created and connected

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,20 @@ import path from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
 import reportRouter from "./src/routes/report.route.js";
+import helmet from "helmet";
+import { connectToDB } from "./src/config/db.js";
 
 const app = express();
 
 // Enable CORS
 app.use(cors());
-
+app.use(express.json());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
 // Serve static files from the "public" directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,11 +25,12 @@ app.set("view engine", "ejs");
 
 app.use("/report", reportRouter);
 
-// app.get("/generate-pdf", async (req, res) => {
-
-// });
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", async () => {
+  try {
+    await connectToDB();
+    console.log(`Server is running on http://localhost:${PORT}`);
+  } catch (e) {
+    console.log("Error in database connection", e);
+  }
 });
