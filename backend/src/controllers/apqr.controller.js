e0b88cd978ca314fd55a4695602326b2b0497766
gaurav <gaurav.meena@vidyagxp.com>
2024-08-24 09:47:21 +0530

    [Refactor] : apqr model , apqr controller

--- backend/src/controllers/apqr.controller.js
+++ backend/src/controllers/apqr.controller.js
@@ -1,19 +1,28 @@
 import { APQR } from "../models/apqr.model.js";
 
 export const createApqr = async (req, res) => {
-  console.log("req.body", req.body);
-
   try {
     const newAPQR = await APQR.create({
+      pqrNo: req.body.pqrNo || 0,
       productName: req.body.productName,
+      productCode: req.body.productCode || [],
       genericName: req.body.genericName,
+      dosageForm: req.body.dosageForm,
       initiator: req.body.initiator,
-      reviewStartDate: new Date(req.body.reviewStartDate), // Use the current date/time
+      reviewStartDate: req.body.reviewStartDate ? new Date(req.body.reviewStartDate) : null,
+      reviewEndDate: req.body.reviewEndDate ? new Date(req.body.reviewEndDate) : null,
+      mfgLicNo: req.body.mfgLicNo,
+      productDescription: req.body.productDescription,
+      processFlow: req.body.processFlow,
+      totalBatchesManufactured: req.body.totalBatchesManufactured || 0,
+      totalBatchesApprovedReleased: req.body.totalBatchesApprovedReleased || 0,
+      totalProcessValidationBatches: req.body.totalProcessValidationBatches || 0,
+      totalReprocessedBatches: req.body.totalReprocessedBatches || 0,
     });
+
     return res.json(newAPQR);
   } catch (error) {
     console.error("Error creating APQR:", error);
-    return res.status(500).json({ error: error.message });  
+    return res.status(500).json({ error: error.message });
   }
 };
-
--- backend/src/models/apqr.model.js
+++ backend/src/models/apqr.model.js
@@ -1,28 +1,88 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/db.js";
+import { msa2 } from "./grids/msa2.model.js";
+import gridRef from "./gridRef.model.js";
 
 export const APQR = sequelize.define("ALL_APQR", {
+  //General Info Datafields
   pqrId: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
   },
+  initiator: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  pqrNo: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0,
+  },
   productName: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: true,
+  },
+  productCode: {
+    type: DataTypes.JSON,
+    allowNull: true,
+    defaultValue: [],
   },
   genericName: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: true,
   },
-  initiator: {
+  dosageForm: {
     type: DataTypes.STRING,
-    allowNull: false,
+    allowNull: true,
   },
   reviewStartDate: {
     type: DataTypes.DATE,
-    allowNull: false,
+    allowNull: true,
   },
+  reviewEndDate: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  mfgLicNo: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  // Manufacturing Review datafields
+  productDescription: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  processFlow: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  totalBatchesManufactured: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0,
+  },
+  totalBatchesApprovedReleased: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0,
+  },
+  totalProcessValidationBatches: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0,
+  },
+  totalReprocessedBatches: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    defaultValue: 0,
+  },
+  // Timestamps
   createdAt: DataTypes.DATE,
   updatedAt: DataTypes.DATE,
+});
+
+APQR.hasMany(gridRef, {
+  foreignKey: "apqrId",
+  as: "mainGrids",
 });
