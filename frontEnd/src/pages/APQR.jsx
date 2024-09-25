bac81dbf6528e2d10962ce2bcc4277da82a2fe19
gaurav <gaurav.meena@vidyagxp.com>
2024-09-25 10:40:37 +0530

    update for  import export

--- frontEnd/src/pages/APQR.jsx
+++ frontEnd/src/pages/APQR.jsx
@@ -262,6 +262,27 @@ export default function APQR() {
     ActualInputinKg5400078000Kg: "actualInput",
     ActualOutputinKgExpectedOutputRange5400078000Kg: "actualOutput",
     ActualInputinKg5000057000Kg: "actualInput",
+    //     SNo
+    // BatchNo
+    // CriticalParameter:01NitrogengaspurgingshouldbecarriedoutinadvanceinAcetonideformationreactionpartI
+    // CriticalParameter:02InBrominationreactionpartII,slowagitationisrequiredduringthereaction
+    // CriticalParameter:03EpoxydationreactionpartIIIrequiredArgongasbubblingtoremoveoxygencontentfromthereactionmass
+    // CriticalParameter:01DuringhandlingofHydrofluoricAcidusenecessaryprotectiveacidproofmask,dressandshoes
+    // CriticalParameter:02AdditionofthematerialshouldbedoneportionwiseduringreactionStdQtyinkg5000057000
+    // CriticalParameter:03Duringadditionofmaterial,exothermicityappears,socontrolthetemperature40°Cto30°CActualTemperatureºC40°Cto30°C
+    // LowerLimit
+    // UpperLimit
+    // CriticalParameter:04Temperatureshouldbewithinspecifiedrangeof20°Cto25°CfortimelycompletionofreactionandcontrolledimpuritylevelActualTemperatureºC20°Cto25°C
+    // CriticalParameter:01additionofSodiumHydroxideandMethanolSolutioninreactionmass,reactionmassandNaOH/MethanolsolutionbothshouldbeoxygenfreebyArgonbubbling
+    // CriticalParameter:02:AdjustpHbetween5060byaddingadequatequantityofGlacialaceticacid
+    // ActualQtofGlacialAceticacid
+    // ActualpHbetween5060
+    // LowerLimit
+    // UpperLimit
+    // CriticalParameter:01Thereactionmassshouldberefluxedfor35hourattemperaturebetween55to60°C
+    // ActualTimeInhrs
+    // ActualTemperature°C55°Cto60°C
+    // CriticalParameter:02Crystallizationoffinishedproductshouldbeat0°Cto5°CActualTemperature°C0°Cto5°C
   };
 
   const setimportedData = (data, gridNo) => {
@@ -340,6 +361,58 @@ export default function APQR() {
         setYieldTOS4([...yieldTOS4, ...processedData]);
         break;
       case 105:
+        console.log(processedData);
+        setYieldTOS5([...yieldTOS5, ...processedData]);
+        break;
+      case 106:
+        console.log(processedData);
+        setYieldTOS5([...yieldTOS5, ...processedData]);
+        break;
+      case 107:
+        console.log(processedData);
+        setYieldTOS5([...yieldTOS5, ...processedData]);
+        break;
+      case 108:
+        console.log(processedData);
+        setTrendingOCPPS1([...trendingOCPPS1, ...processedData]);
+        break;
+      case 109:
+        console.log(processedData);
+        setTrendingOCPPS2([...trendingOCPPS2, ...processedData]);
+        break;
+      case 110:
+        console.log(processedData);
+        setTrendingOCPPS3([...trendingOCPPS3, ...processedData]);
+        break;
+      case 111:
+        console.log(processedData);
+        setTrendingOCPPS4([...trendingOCPPS4, ...processedData]);
+        break;
+      case 112:
+        console.log(processedData);
+        setTrendingOIPIPS1([...trendingOIPIPS1, ...processedData]);
+        break;
+      case 113:
+        console.log(processedData);
+        setTrendingOIPIPS2([...trendingOIPIPS2, ...processedData]);
+        break;
+      case 114:
+        console.log(processedData);
+        setTrendingOIPIPS3([...trendingOIPIPS3, ...processedData]);
+        break;
+      case 115:
+        console.log(processedData);
+        setTrendingOIPIPS4([...trendingOIPIPS4, ...processedData]);
+        break;
+      case 116:
+        console.log(processedData);
+        setTrendingOIPIPS5([...trendingOIPIPS5, ...processedData]);
+        break;
+      case 117:
+        console.log(processedData);
+        setYieldTOS5([...yieldTOS5, ...processedData]);
+        break;
+      case 118:
         console.log(processedData);
         setYieldTOS5([...yieldTOS5, ...processedData]);
         break;
@@ -12715,9 +12788,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOCPPS1}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOCPPS1.xlsx"
+                    gridNo={108}
+                    sheetNumber={8}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -12804,9 +12880,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOCPPS2}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOCPPS2.xlsx"
+                    gridNo={109}
+                    sheetNumber={9}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -12936,9 +13015,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOCPPS3}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOCPPS3.xlsx"
+                    gridNo={110}
+                    sheetNumber={10}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -13048,9 +13130,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOCPPS4}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOCPPS4.xlsx"
+                    gridNo={111}
+                    sheetNumber={11}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -13185,9 +13270,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOIPIPS1}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOIPIPS1.xlsx"
+                    gridNo={112}
+                    sheetNumber={12}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -13444,9 +13532,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOIPIPS2}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOIPIPS2.xlsx"
+                    gridNo={113}
+                    sheetNumber={13}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -13662,9 +13753,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOIPIPS3}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOIPIPS3.xlsx"
+                    gridNo={114}
+                    sheetNumber={14}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -13814,9 +13908,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOIPIPS4}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOIPIPS4.xlsx"
+                    gridNo={115}
+                    sheetNumber={15}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -14068,7 +14165,7 @@ export default function APQR() {
             </div>
 
             <div className="gridName pt-4">
-              Trending of In-process & Intermediate Parameter of U3TCAg - Stage-V{" "}
+              Trending of In-process & Intermediate Parameter of U3TCAg - Stage-V
             </div>
             <div>
               <div className="AddRows d-flex w-full justify-between items-center text-3xl">
@@ -14078,9 +14175,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={trendingOIPIPS5}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="trendingOIPIPS5.xlsx"
+                    gridNo={116}
+                    sheetNumber={16}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
@@ -14288,15 +14388,20 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={yieldTOS5}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="yieldTOS5.xlsx"
+                    gridNo={117}
+                    sheetNumber={17}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
               <table>
                 <thead>
-          <tr>                    <th rowSpan="2">S. No.</th>
+                  <tr>
+                    {" "}
+                    <th rowSpan="2">S. No.</th>
                     <th rowSpan="2">Batch No.</th>
                     <th rowSpan="2">Specific Rotation b/w +118° and +130° </th>
                     <th rowSpan="2">L Limit</th>
@@ -14321,17 +14426,15 @@ export default function APQR() {
                     <th>Limit</th>
                     <th>Total Impurities NMT 0.5%</th>
                     <th>Limit</th>
-
                     <th>Methanol NMT 1500 ppm</th>
                     <th>Acetone NMT 2000 ppm</th>
                     <th>Methylene chloride NMT 400 ppm</th>
                     <th>Limit</th>
-
                     <th>{"90%  < 10 μm"}</th>
                     <th>Limit</th>
                     <th>{"99.5% < 20 μm"}</th>
-                    <th>Limit</th>
-                  s</tr>
+                    <th>Limit</th>s
+                  </tr>
                 </thead>
                 <tbody>
                   {yieldTOS1.map((item, index) => {
@@ -14630,9 +14733,12 @@ export default function APQR() {
                 </div>
                 <div className="flex gap-4 ">
                   <ExcelExportImport
-                    data={currentRPQRN}
+                    data={yieldTOS5}
                     setimportedData={setimportedData}
-                    fileName="currentRPQRN.xlsx"
+                    fileName="yieldTOS5.xlsx"
+                    gridNo={118}
+                    sheetNumber={18}
+                    headerRowNumber={1}
                   />
                 </div>
               </div>
